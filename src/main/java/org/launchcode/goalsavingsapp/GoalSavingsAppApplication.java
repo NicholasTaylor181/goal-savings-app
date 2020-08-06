8850a5f3681fe1855b766dd8d956f0edd709ac38
Nick <nicholastaylor181@gmail.com>
2020-08-06 18:52:25 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/goalsavingsapp/GoalSavingsAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.goalsavingsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalSavingsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalSavingsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/goalsavingsapp/GoalSavingsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.goalsavingsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalSavingsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
